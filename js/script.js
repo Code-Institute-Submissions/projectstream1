66d8c5f40104a3047916d692a60b4468a430c9a6
CristianBurloiu <cristian.burloiu@yahoo.ro>
2018-01-31 11:49:55 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
+
+
+$(function() {
+  $(document).on('click', 'a.page-scroll', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
+});
