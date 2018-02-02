c6a6f2f89e0a4dfa4e94ceccc15c8a6534d0e40a
CristianBurloiu <cristian.burloiu@yahoo.ro>
2018-02-02 15:45:23 +0000

    Adopt section JS and 60% layout complete

--- js/script.js
+++ js/script.js
@@ -8,13 +8,27 @@ hamburger.addEventListener("click", function() {
   // Do something else, like open/close menu
 });
 
+ 
+        function adopt() {
+            let pets=[
+            '<img src="images/1.jpeg">',
+            '<img src="images/2.jpg">', 
+            '<img src="images/3.jpg">',
+            '<img src="images/4.jpg">',
+            '<img src="images/5.jpeg">',
+            '<img src="images/6.png">',
+            '<img src="images/7.jpeg">',
+            '<img src="images/8.jpg">'];
+            let indexno=Math.floor((Math.random() * 8));
+            document.getElementById("adopt_image").innerHTML = pets[indexno];
+        }
+    
+        
 
-$(function() {
-  $(document).on('click', 'a.page-scroll', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-  });
-});
+$(".adopt_button").click(function(){
+  $("#writehere").css("display", "block")
+})
+
+$(document).ready(function() {
+  $("#adopt_right").css("height", $("#adopt_left").height());
+})
